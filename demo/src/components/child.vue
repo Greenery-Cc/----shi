056312277c08a417ff9d8e6a42735ebbac47fddd
NewbieOnTheRoad <350019619@qq.com>
2019-11-08 15:43:14 +0800

    Initial commit

---
+++ demo/src/components/child.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        child 
+
+        tit === {{tit}}
+
+        <button @click="$emit('get',con)">子向父传递数据</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['tit'],
+    data(){
+        return {
+            con:'子级的数据'
+        }
+    },
+    mounted() {
+        console.log(this.$parent.title)
+    },
+}
+</script>
---
+++ demo/src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="home">
+        <Child :tit="title" @get="getCon" ref="child"></Child>
+        <InputTxt></InputTxt>
+        <List></List>
+        <div ref="red">红色的box</div>
+        <input type="text" v-focus v-bind:value="title" @input="ipt">
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Child from '@/components/child'
+import InputTxt from '@/components/input-txt'
+import List from '@/components/list'
+export default {
+    name: 'home',
+    data(){
+        return {
+            title:'父组件的数组'
+        }
+    },
+    components: {
+        Child,
+        InputTxt,
+        List
+    },
+    mounted() {
+        // console.log("chilren",this.$children[0].con)
+        console.log(this.$refs.child.con)
+        this.$refs.red.style.background = 'red';
+    },
+    methods:{
+        getCon(con){
+            console.log(con)
+        },
+        ipt(event){
+            this.title = event.target.value;
+        }
+    },
+    // 注册指令
+    directives:{
+        focus: {
+            // 指令的定义
+            inserted: function (el) {
+                el.focus()
+            }
+        }
+    }
+}
+</script>
